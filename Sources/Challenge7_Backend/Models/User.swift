05aca68fc1d418edc0de70072473a286b5f67dce
João Vitor Rocha Miranda <jv@MacBook-Pro-de-JV.local>
2025-08-19 16:08:40 -0300

    Created user model

---
+++ Sources/Challenge7_Backend/Models/User.swift
@@ -0,0 +1,66 @@
+//
+//  File.swift
+//  Challenge7_Backend
+//
+//  Created by João Vitor Rocha Miranda on 19/08/25.
+//
+
+import Foundation
+import Vapor
+import Fluent
+
+//MARK: - Enums
+
+///Enumerates different types (permissions) of users
+enum UserRole: String, Codable {
+    case adm
+    case mentor
+    case student
+}
+
+///Enumerates differents types of user path of interest
+enum UserPath: String, Codable {
+    case design
+    case code
+    case undefined
+}
+
+//MARK: - User Model
+
+final class User: Model, @unchecked Sendable{
+    
+    static let schema = "TB_users"
+    
+    /// Unique identifier for this user.
+    @ID(key: .id)
+    var id: UUID?
+    
+    /*User Atributes*/
+    @Field(key: "name")
+    var name: String
+    
+    @Field(key: "email")
+    var email: String
+    
+    @Field(key: "password")
+    var password: String
+    
+    @Field(key: "role")
+    var role: UserRole
+    
+    @Field(key: "path")
+    var path: UserPath
+    
+    
+    ///Class constructor
+    init() {}
+    init(id: UUID? = nil, name: String, email: String, password: String, role: UserRole, path: UserPath) {
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.role = role
+        self.path = path
+    }
+    
+}
