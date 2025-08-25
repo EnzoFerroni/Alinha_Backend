b6d590c5952d3222c6ad0dd80316f6ab41f8f052
João Vitor Rocha Miranda <jv@MacBook-Pro-de-JV.local>
2025-08-25 17:59:58 -0300

    Created fetch and delete

--- Sources/Challenge7_Backend/Controllers/UserOrganizationController.swift
+++ Sources/Challenge7_Backend/Controllers/UserOrganizationController.swift
@@ -8,7 +8,19 @@
 import Vapor
 
 final class UserOrganizationController{
-    func index(req: Request) async throws -> [UserOrganizationDTO] {
-        try await UserOrganization.query(on: req.db).all().map { $0.toDTO() }
+    var userOrganizations: [UserOrganizationDTO]?
+    
+    func fetchUserOrg(req: Request) async throws{
+        userOrganizations = try await UserOrganization.query(on: req.db).all().map { $0.toDTO() }
     }
+    
+    func delete(req: Request) async throws{
+        //TODO: IMPLEMENT MIDDLEWARE
+        guard let userOrg = try await UserOrganization.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        try await userOrg.delete(on: req.db)
+    }
+    
+    
 }
