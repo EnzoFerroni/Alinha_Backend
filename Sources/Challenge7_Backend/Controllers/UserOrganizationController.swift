ea3c89a508a59a9bd9e77d49aa7daad796c4def1
João Vitor Rocha Miranda <jv@MacBook-Pro-de-JV.local>
2025-08-25 17:36:25 -0300

    fixed controller

--- Sources/Challenge7_Backend/Controllers/UserOrganizationController.swift
+++ Sources/Challenge7_Backend/Controllers/UserOrganizationController.swift
@@ -7,21 +7,8 @@
 
 import Vapor
 
-struct UserController: RouteCollection{
-    func boot(routes: any RoutesBuilder) throws{
-        let userControllers = routes.grouped("userControllers")
-        userControllers.get(use: index)
-        //userControllers.post(use: create)
-        
-        userControllers.group(":id"){userController in
-            //.get(use: show)
-            //userControllers.put(use: update)
-            //userControllers.delete(use: delete)
-        }
-    }
-    
+final class UserOrganizationController{
     func index(req: Request) async throws -> [UserOrganizationDTO] {
         try await UserOrganization.query(on: req.db).all().map { $0.toDTO() }
     }
-    
 }
