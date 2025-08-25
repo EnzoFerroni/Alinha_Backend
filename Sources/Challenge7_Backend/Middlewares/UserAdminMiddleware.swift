c2f3397c5bede09964967c078aed8ff388331b5c
Pedro Augusto <tesssaro@10-44-48-26.0200.mack.br.iosda.org>
2025-08-25 17:47:44 -0300

    feat:

--- Sources/Challenge7_Backend/Middlewares/UserAdminMiddleware.swift
+++ Sources/Challenge7_Backend/Middlewares/UserAdminMiddleware.swift
@@ -1,9 +1,18 @@
 import Vapor
 
-///Midaware that garantees that the user is an adm
+/////Midaware that garantees that the user is an adm
+//struct UserAdminMiddleware: Middleware {
+//    func respond(to request: Request, chainingTo next: any Responder) -> EventLoopFuture<Response> {
+//        guard let user = request.auth.get(User.self), user.role == .adm else {
+//            return request.eventLoop.future(error: Abort(.unauthorized))
+//        }
+//        return next.respond(to: request)
+//    }
+//}
+
 struct UserAdminMiddleware: Middleware {
     func respond(to request: Request, chainingTo next: any Responder) -> EventLoopFuture<Response> {
-        guard let user = request.auth.get(User.self), user.role == .adm else {
+        guard let user = request.auth.get(User.self), user.userOrganization?.user_role == .adm else {
             return request.eventLoop.future(error: Abort(.unauthorized))
         }
         return next.respond(to: request)
