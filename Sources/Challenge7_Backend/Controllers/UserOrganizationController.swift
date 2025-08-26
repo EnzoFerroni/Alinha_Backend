05128509ed51fb68df005eecd6c5b5ccb3da2f02
João Vitor Rocha Miranda <jv@MacBook-Pro-de-JV.local>
2025-08-26 15:12:04 -0300

    Created userOrganizationController

--- Sources/Challenge7_Backend/Controllers/UserOrganizationController.swift
+++ Sources/Challenge7_Backend/Controllers/UserOrganizationController.swift
@@ -4,23 +4,41 @@
 //
 //  Created by João Vitor Rocha Miranda on 25/08/25.
 //
-
+import Fluent
 import Vapor
 
-final class UserOrganizationController{
-    var userOrganizations: [UserOrganizationDTO]?
+class UserOrganizationController{
     
-    func fetchUserOrg(req: Request) async throws{
-        userOrganizations = try await UserOrganization.query(on: req.db).all().map { $0.toDTO() }
+    func index(req: Request) async throws -> [UserOrganizationDTO] {
+        try await UserOrganization.query(on: req.db).all().map { $0.toDTO() }
     }
     
-    func delete(req: Request) async throws{
-        //TODO: IMPLEMENT MIDDLEWARE
+    
+    func create(org: Organization, user: User, role: UserRole, database: Database) async throws -> UserOrganization {
+        
+        let userOrg = UserOrganization(
+            org_id: org,
+            user_id: user,
+            user_role: role
+        )
+        
+        try await userOrg.save(on: database)
+        return userOrg
+    }
+    
+    func show(req: Request) async throws -> UserOrganizationDTO {
+        guard let userOrg = try await UserOrganization.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        return userOrg.toDTO()
+    }
+    
+    
+    func delete(req: Request) async throws -> HTTPStatus {
         guard let userOrg = try await UserOrganization.find(req.parameters.get("id"), on: req.db) else {
             throw Abort(.notFound)
         }
         try await userOrg.delete(on: req.db)
+        return .ok
     }
-    
-    
 }
