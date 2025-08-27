dcbda7a9533f8bc4ab639120b83876aed2cd3c88
Dayo Araujo <dayo.araujosz@gmail.com>
2025-08-27 14:58:29 -0300

    fix: appointment relations

--- Sources/Challenge7_Backend/Models/Organization.swift
+++ Sources/Challenge7_Backend/Models/Organization.swift
@@ -32,7 +32,7 @@ final class Organization: Model, @unchecked Sendable {
     @Field(key: "token")
     var token: String
     
-    @OptionalParent(key: "appointment_id")
+    @OptionalChild(for: \.$organization)
     var appointment: Appointment?
     
     @Field(key: "appointment_places")
