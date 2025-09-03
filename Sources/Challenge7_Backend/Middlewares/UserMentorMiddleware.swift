14de5b20718fbd1b676ecfc68158efb03d18f7e3
J040VRM <joaovitormbj@gmail.com>
2025-09-03 15:18:06 -0300

    docs: Middleware doc

--- Sources/Challenge7_Backend/Middlewares/UserMentorMiddleware.swift
+++ Sources/Challenge7_Backend/Middlewares/UserMentorMiddleware.swift
@@ -8,11 +8,14 @@
 import Vapor
 import Fluent
 
+///Middleware that filters if the requesting user is an ADM or Mentor
 struct AdmMentorMiddleware: Middleware {
     func respond(to req: Request, chainingTo next: any Responder) -> EventLoopFuture<Response> {
+        ///Check if the iser is logged
         guard let user = req.auth.get(User.self) else {
             return req.eventLoop.future (error: Abort(.unauthorized, reason: "Not authenticated"))
         }
+        ///Check if the role is adm or mentor
         guard user.role == .mentor || user.role == .adm else {
             return req.eventLoop.future (error: Abort(.forbidden, reason: "Admins only"))
         }
